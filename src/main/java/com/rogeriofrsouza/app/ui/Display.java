6aacc648e7c64b2748458621e4eec6a5f08892cd
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2025-07-22 09:23:23 -0300

    refactor(ui): safe delete methods and tests

--- src/main/java/com/rogeriofrsouza/app/ui/Display.java
+++ src/main/java/com/rogeriofrsouza/app/ui/Display.java
@@ -2,11 +2,8 @@ package com.rogeriofrsouza.app.ui;
 
 import com.rogeriofrsouza.app.chess.ChessMatch;
 import com.rogeriofrsouza.app.chess.ChessPiece;
-import com.rogeriofrsouza.app.chess.ChessPosition;
 
-import java.util.InputMismatchException;
 import java.util.List;
-import java.util.Scanner;
 
 public class Display {
 
@@ -72,39 +69,5 @@ public class Display {
 
         stringBuilder.append("  a b c d e f g h");
         System.out.println(stringBuilder);
-    }
-
-    private String readInput(Scanner scanner) {
-        String input = scanner.nextLine();
-
-        if (input == null || input.trim().isBlank()) {
-            throw new InputMismatchException("Error reading input: cannot be null or blank.");
-        }
-
-        return input.trim();
-    }
-
-    public ChessPosition readChessPosition(Scanner scanner) {
-        String input = readInput(scanner);
-
-        if (!input.matches("[a-h][1-8]")) {
-            throw new InputMismatchException(
-                "Error reading ChessPosition. Valid values are from a1 to h8.");
-        }
-
-        char column = input.charAt(0);
-        int row = Character.getNumericValue(input.charAt(1));
-
-        return new ChessPosition(column, row);
-    }
-
-    public ChessPiece.Name readPromotedPiece(Scanner scanner) {
-        String input = readInput(scanner).substring(0, 1);
-
-        return ChessMatch.possiblePromotedPieces
-            .stream()
-            .filter(pieceName -> pieceName.getLetter().equalsIgnoreCase(input))
-            .findFirst()
-            .orElse(null);
     }
 }
