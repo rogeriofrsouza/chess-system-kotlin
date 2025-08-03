dbcb8d45d8c9b72b90f464de8e3fda762d85e921
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2025-08-03 10:32:22 -0300

    feat(chess.pieces): add knight move directions and change constructor

--- src/main/java/com/rogeriofrsouza/app/chess/pieces/Knight.java
+++ src/main/java/com/rogeriofrsouza/app/chess/pieces/Knight.java
@@ -2,13 +2,22 @@ package com.rogeriofrsouza.app.chess.pieces;
 
 import com.rogeriofrsouza.app.boardgame.Board;
 import com.rogeriofrsouza.app.boardgame.Position;
+import com.rogeriofrsouza.app.chess.ChessMoveDirection;
 import com.rogeriofrsouza.app.chess.ChessPiece;
 import com.rogeriofrsouza.app.chess.Color;
+import com.rogeriofrsouza.app.chess.Name;
+
+import java.util.List;
 
 public class Knight extends ChessPiece {
 
+    private static final List<ChessMoveDirection> CHESS_MOVE_DIRECTIONS = ChessMoveDirection.getEntries()
+            .stream()
+            .filter(c -> c.name().startsWith("KNIGHT"))
+            .toList();
+
     public Knight(Board board, Color color) {
-        super(board, color);
+        super(board, color, Name.KNIGHT, CHESS_MOVE_DIRECTIONS);
     }
 
     @Override
