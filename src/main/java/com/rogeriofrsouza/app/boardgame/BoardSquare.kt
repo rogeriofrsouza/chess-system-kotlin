b26a7003365d4eba42d7f76338fa672a8f74d478
Rogério Ferreira de Souza <rogeriofrsouza@gmail.com>
2025-08-12 21:05:24 -0300

    feat(boards): add property, getter and remove primary constructor

--- src/main/java/com/rogeriofrsouza/app/boardgame/BoardSquare.kt
+++ src/main/java/com/rogeriofrsouza/app/boardgame/BoardSquare.kt
@@ -1,3 +1,7 @@
 package com.rogeriofrsouza.app.boardgame
 
-class BoardSquare(var piece: Piece? = null)
+class BoardSquare {
+
+    var piece: Piece? = null
+    var isPossibleMove: Boolean = false
+}
